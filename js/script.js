dd9a648b7bc126d67c5b17418abadce47c137b7e
martinthoresen <87776525+martinthoresen@users.noreply.github.com>
2022-04-03 22:39:49 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,70 @@
 const cats = [
-    {
-        name: "Blob",
-        age: 10
-    },
-    {
-        name: "Harold",
-    },
-    {
-        name: "Blurt",
-        age: 21
-    }
+  {
+    name: "Blob",
+    age: 10,
+  },
+  {
+    name: "Harold",
+  },
+  {
+    name: "Blurt",
+    age: 21,
+  },
 ];
 
+// question 1
+
+const cat = {
+  complain: function () {
+    console.log("Meow!");
+  },
+};
+
+// question 2
+
+const heading = document.querySelector("h3");
+
+heading.innerHTML = "Updated Heading";
+
+// question 3
+
+heading.style.fontSize = "2em";
+
+// question 4
+
+heading.classList.add("subheading");
+
+// question 5
+
+const paragraphs = document.querySelectorAll("p");
+
+paragraphs.forEach((element) => (element.style.color = "red"));
+
+// question 6
+
+const resultsContainer = document.querySelector(".results");
+
+resultsContainer.innerHTML = "<p>New Paragraph</p>";
+resultsContainer.style.backgroundColor = "yellow";
+
+// question 7
+
+function logName(list) {
+  list.forEach((cat) => console.log(cat.name));
+}
+
+logName(cats);
+
+// question 8
+const catContainer = document.querySelector(".cat-container");
+function createCats(cats) {
+  cats.forEach(
+    (cat) =>
+      (catContainer.innerHTML += `<div>
+                                            <h5>${cat.name}</h5>
+                                            <p>${cat.age}</p>
+                                            </div>`)
+  );
+}
+
+createCats(cats);
